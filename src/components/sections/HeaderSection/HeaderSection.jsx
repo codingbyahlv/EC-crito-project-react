cef65ec72de417783e33b311a47c517bd6b7b93b
codingbyahlv <code@ahlv.se>
2023-11-13 16:45:18 +0100

    mobile menu

--- src/components/sections/HeaderSection/HeaderSection.jsx
+++ src/components/sections/HeaderSection/HeaderSection.jsx
@@ -8,8 +8,44 @@ import SocialMedia from "@shared/SocialMedia/SocialMedia";
 import "@styles/main.scss";
 import "@shared/Buttons/Button.scss";
 import "./HeaderSection.scss";
+import { useState } from "react";
+import HamburgerMenu from "react-hamburger-menu";
 
 const HeaderSection = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const MobileMenu = () => {
+    return (
+      <div className="mobileMenu">
+        <nav className="navigationWrapper">
+          <NavLink to="/" className="link" onClick={toggleMenu}>
+            Home
+          </NavLink>
+          <NavLink to="/service" className="link" onClick={toggleMenu}>
+            Service
+          </NavLink>
+          <NavLink to="/news" className="link" onClick={toggleMenu}>
+            News
+          </NavLink>
+          <NavLink to="/contact" className="link" onClick={toggleMenu}>
+            Contact
+          </NavLink>
+          <ButtonLink
+            value="Login"
+            to="/login"
+            iconName="arrow"
+            className="btnYellow loginBtn mobileLoginBtn"
+            onClick={toggleMenu}
+          />
+        </nav>
+      </div>
+    );
+  };
+
   return (
     <header className="headerWrapper">
       <div className="contentWrapper">
@@ -44,6 +80,19 @@ const HeaderSection = () => {
             />
           </div>
         </div>
+        {isOpen && <MobileMenu />}
+        <HamburgerMenu
+          isOpen={isOpen}
+          menuClicked={toggleMenu}
+          width={40}
+          height={25}
+          strokeWidth={3}
+          rotate={0}
+          color="black"
+          borderRadius={0}
+          animationDuration={0.5}
+          className="hamburger"
+        />
       </div>
     </header>
   );
--- src/components/shared/Buttons/ButtonLink.jsx
+++ src/components/shared/Buttons/ButtonLink.jsx
@@ -3,11 +3,11 @@ import IconRenderer from "../IconRenderer/IconRenderer";
 import "@styles/main.scss";
 import "./Button.scss";
 
-const ButtonLink = ({ value, to, iconName, className }) => {
+const ButtonLink = ({ value, to, iconName, className, onClick }) => {
   const combinedClassName = `button ${className}`;
 
   return (
-    <NavLink to={to} className={combinedClassName}>
+    <NavLink to={to} className={combinedClassName} onClick={onClick}>
       {value}
       <IconRenderer iconName={iconName} className="" />
     </NavLink>
